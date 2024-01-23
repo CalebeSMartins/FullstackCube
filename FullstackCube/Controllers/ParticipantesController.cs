d4bbd3d9b3c1c6102a6a8d5107ccae67bc75e1c0
caleb <caleb@DESKTOP-K0V5AD6>
2024-01-22 23:38:34 -0300

    Adicionar arquivos de projeto.

---
+++ FullstackCube/Controllers/ParticipantesController.cs
@@ -0,0 +1,27 @@
+﻿using FullstackCube.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FullstackCube.Controllers
+{
+    [Route("api/participantes")]
+    [ApiController]
+    public class ParticipantesController : ControllerBase
+    {
+        public ParticipantesController()
+        {
+            
+        }
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            var participante = new Participante();
+            participante.Id = 1;
+            participante.FirstName = "Calebe";
+            participante.LastName = "Martins";
+            participante.Participation = 5;
+
+            return Ok(participante);
+        }
+    }
+}
---
+++ FullstackCube/Models/Participante.cs
@@ -0,0 +1,11 @@
+﻿namespace FullstackCube.Models
+{
+    public class Participante
+    {
+        
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Participation { get; set; }
+    }
+}
